de6f63e3b2915081cd5a5eb604c2a058aaec68ba
nicolas.heldenbergh@gmail.com <nicolas.heldenbergh@gmail.com>
2023-11-27 11:53:39 +0100

    Housing-location commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HousingLocationComponent} from "../housing-location/housing-location.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
