b59f8b0911aafcb1b97f5ddfcc35a842136cab2a
nicolas.heldenbergh@gmail.com <nicolas.heldenbergh@gmail.com>
2023-11-27 16:12:08 +0100

    housing service and details commit

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,23 +1,37 @@
 import {Component, inject} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {ActivatedRoute} from "@angular/router";
 import {HousingService} from "../housing.service";
 import {HousingLocation} from "../housing-location";
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
-  selector: 'app-details',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: 'details.component.html',
-  styleUrls: ['./details.component.css']
+    selector: 'app-details',
+    standalone: true,
+    imports: [CommonModule, ReactiveFormsModule],
+    templateUrl: 'details.component.html',
+    styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-  route: ActivatedRoute = inject(ActivatedRoute);
-  housingService = inject(HousingService);
-  housingLocation: HousingLocation | undefined;
+    route: ActivatedRoute = inject(ActivatedRoute);
+    housingService = inject(HousingService);
+    housingLocation: HousingLocation | undefined;
+    applyForm = new FormGroup({
+        firstName: new FormControl(''),
+        lastName: new FormControl(''),
+        email: new FormControl('')
+    });
 
-  constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
-  }
+    constructor() {
+        const housingLocationId = Number(this.route.snapshot.params['id']);
+        this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    }
+
+    submitApplication() {
+        this.housingService.submitApplication(
+            this.applyForm.value.firstName ?? '',
+            this.applyForm.value.lastName ?? '',
+            this.applyForm.value.email ?? ''
+        );
+    }
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HousingLocation} from "./housing-location";
+import {first} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -117,4 +118,7 @@ export class HousingService {
   getHousingLocationById(id: Number): HousingLocation | undefined {
   return this.housingLocationList.find(housingLocation => housingLocation.id === id);
 }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(firstName, lastName, email);
+  }
 }
